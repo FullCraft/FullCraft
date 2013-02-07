5b682d7a1529c9a60ef1edf2fdfd0e0a4fbde810
Eduardo <Eduardo@Eduardo-PC>
2013-02-07 06:21:17 -0200

    Main File

---
+++ FullCraft/src/com/fullcraft/FullCraft.java
@@ -0,0 +1,12 @@
+package com.fullcraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FullCraft extends JavaPlugin{
+    @Override
+    public void onEnable() {
+    }
+    @Override
+    public void onDisable() {
+    }
+}
